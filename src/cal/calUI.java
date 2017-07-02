2bac630a452635edcf1decfe0a36f7086f9ab057
Admin <Admin@Lyine>
2017-07-02 23:38:17 +0900

    계산기 기본 기능 구현 완료

--- src/cal/calUI.java
+++ src/cal/calUI.java
@@ -6,37 +6,48 @@ public class calUI {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner s = new Scanner(System.in);
+			Scanner s = new Scanner(System.in);
+			int count = 1;
+	        System.out.println(count + "번째 입력 값 : ");
 
-        System.out.println("첫번째 입력 값 : ");
+	        int op1 = s.nextInt();
+	        int result = op1;
+	        while(true){
+	        	count ++;
+	        	System.out.println("연산기호 입력 :");
 
-        int op1 = s.nextInt();
-        
-        System.out.println("연산기호 입력 :");
-
-        String symbol = s.next();
-        
-        System.out.println("두번째 입력 값 : ");
-        
-        int op2 = s.nextInt();
-        
-        cal c1 = new cal();
-        
-        if(symbol.equals("+")){
-        	 System.out.println("덧셈 결과 : " + c1.sum(op1, op2));
-        }
-        else if(symbol.equals("-")){
-        	 System.out.println("뻴셈 결과 : " + c1.sub(op1, op2));
-        }
-        else if(symbol.equals("*")){
-        	System.out.println("곱셈 결과 : " + c1.mul(op1, op2));
-        }
-        else if(symbol.equals("/")){
-        	System.out.println("나눗셈 결과 : " + c1.div(op1, op2));
-        }
-        else {
-        	System.out.println("사친연산이 잘못되었습니다.");
-        }
+		        String symbol = s.next();
+		        
+		        if(symbol.equals("quit")){
+		        	System.out.println("최종 결과 값 : " + result);
+		        	break;
+		        }
+		        
+		        System.out.println(count +"번째 입력 값 : ");
+		        
+		        int op2 = s.nextInt();
+		        
+		        cal c1 = new cal();
+		        
+		        if(symbol.equals("+")){
+		        	result = c1.sum(result, op2);
+		        	 System.out.println("덧셈 결과 : " + result);
+		        }
+		        else if(symbol.equals("-")){
+		        	result =  c1.sub(result, op2);
+		        	 System.out.println("뻴셈 결과 : " + result);
+		        }
+		        else if(symbol.equals("*")){
+		        	result = c1.mul(result, op2);
+		        	System.out.println("곱셈 결과 : " + result);
+		        }
+		        else if(symbol.equals("/")){
+		        	result = c1.div(result, op2);
+		        	System.out.println("나눗셈 결과 : " + result);
+		        }
+		        else {
+		        	System.out.println("사친연산 기호가 잘못되었습니다.");
+		        }
+	        }
 	}
-
 }
