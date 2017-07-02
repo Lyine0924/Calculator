8e535af0678fd19bb17a9cb03250839ddd4b0daa
Admin <Admin@Lyine>
2017-07-02 23:00:38 +0900

    연산기호를 입력하는 사칙연산 수정

--- src/cal/calUI.java
+++ src/cal/calUI.java
@@ -8,20 +8,35 @@ public class calUI {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 
-        System.out.println("첫번째 입력 값.");
+        System.out.println("첫번째 입력 값 : ");
 
         int op1 = s.nextInt();
         
-        System.out.println("두번째 입력 값.");
+        System.out.println("사칙연산 입력 값 :");
+
+        String symbol = s.next();
+        
+        System.out.println("두번째 입력 값 : ");
         
         int op2 = s.nextInt();
         
         cal c1 = new cal();
         
-        System.out.println("덧셈 결과 : " + c1.sum(op1, op2));
-        System.out.println("뻴셈 결과 : " + c1.sub(op1, op2));
-        System.out.println("곱셈 결과 : " + c1.mul(op1, op2));
-        System.out.println("나눗셈 결과 : " + c1.div(op1, op2));
+        if(symbol.equals("+")){
+        	 System.out.println("덧셈 결과 : " + c1.sum(op1, op2));
+        }
+        else if(symbol.equals("-")){
+        	 System.out.println("뻴셈 결과 : " + c1.sub(op1, op2));
+        }
+        else if(symbol.equals("*")){
+        	System.out.println("곱셈 결과 : " + c1.mul(op1, op2));
+        }
+        else if(symbol.equals("/")){
+        	System.out.println("나눗셈 결과 : " + c1.div(op1, op2));
+        }
+        else {
+        	System.out.println("사친연산이 잘못되었습니다.");
+        }
 	}
 
 }
