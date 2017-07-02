c613472f4b7a9529a55efa29f3e19a4e52a6659c
Admin <Admin@Lyine>
2017-07-02 23:00:07 +0900

    두 개의 값을 입력받아 사칙연산 구현

--- src/cal/calUI.java
+++ src/cal/calUI.java
@@ -4,26 +4,24 @@ import java.util.Scanner;
 
 public class calUI {
 	
-	int inop1;
-	int inop2;
-	String symbol;
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 
-        System.out.println("화면에 숫자를 입력하세요.");
+        System.out.println("첫번째 입력 값.");
 
-        int inop1 = s.nextInt();
+        int op1 = s.nextInt();
         
-        int inop2 = s.nextInt();
+        System.out.println("두번째 입력 값.");
+        
+        int op2 = s.nextInt();
         
         cal c1 = new cal();
         
-        System.out.println("덧셈 결과 : " + c1.sum(inop1, inop2));
-        System.out.println("뻴셈 결과 : " + c1.sub(inop1, inop2));
-        System.out.println("곱셈 결과 : " + c1.mul(inop1, inop2));
-        System.out.println("나눗셈 결과 : " + c1.div(inop1, inop2));
+        System.out.println("덧셈 결과 : " + c1.sum(op1, op2));
+        System.out.println("뻴셈 결과 : " + c1.sub(op1, op2));
+        System.out.println("곱셈 결과 : " + c1.mul(op1, op2));
+        System.out.println("나눗셈 결과 : " + c1.div(op1, op2));
 	}
 
 }
